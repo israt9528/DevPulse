8748c6b8ad46224c56fa6e7f10ee23ac849bdb69
israt <israt9528@gmail.com>
2026-05-22 02:00:26 +0600

    add global error handler

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,10 @@
 export const role = ["contributor", "maintainer"] as const;
 
 export type Role = (typeof role)[number];
+
+export type User = {
+  name: string;
+  email: string;
+  password: string;
+  role: string;
+};
